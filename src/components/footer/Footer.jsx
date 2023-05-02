068fcceeaaf3236ae65f5f8af8d2dd2e30a55419
Otabek0417 <otabeksodiqov654@gmail.com>
2023-05-02 18:56:11 +0500

    create user

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <a href="https://github.com/">Otabek Sodoqiv GitHub</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
